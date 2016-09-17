6a96ece6c2ccd8f8a457e16d22672e44d61147ed
Jesse Allen <Will-of-the-Wisp@users.noreply.github.com>
2016-09-17 19:10:49 -0400

    Updated for use as panes.
    Added isPaneVisible() function.

--- js/pane.js
+++ js/pane.js
@@ -1,23 +1,25 @@
-var $animation_elements = $('.pane');
+
+/* Find any panes within the bounds of the window. */
+var $panes = $('.pane');
 var $window = $(window);
 
-function check_if_in_view() {
+function isPaneVisible() {
   var window_height = $window.height();
   var window_top_position = $window.scrollTop();
   var window_bottom_position = (window_top_position + window_height);
 
-  $.each($animation_elements, function() {
-    var $element = $(this);
-    var element_height = $element.outerHeight();
-    var element_top_position = $element.offset().top;
-    var element_bottom_position = (element_top_position + element_height);
+  $.each($panes, function() {
+    var $pane = $(this);
+    var pane_height = $pane.outerHeight();
+    var pane_top_position = $pane.offset().top;
+    var pane_bottom_position = (pane_top_position + pane_height);
 
     //check to see if this current container is within viewport
-    if ((element_bottom_position >= window_top_position) &&
-      (element_top_position <= window_bottom_position)) {
-      $element.addClass('in-view');
+    if ((pane_bottom_position >= window_top_position) &&
+      (pane_top_position <= window_bottom_position)) {
+      $pane.addClass('in-view');
     } else {
-      $element.removeClass('in-view');
+      $pane.removeClass('in-view');
     }
   });
 }

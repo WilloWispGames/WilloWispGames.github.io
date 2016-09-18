52c02839cbb577a4f35dea6cdbe6bdd6cf896deb
Jesse Allen <Will-of-the-Wisp@users.noreply.github.com>
2016-09-17 20:58:39 -0400

    Added initPanes() function

--- js/pane.js
+++ js/pane.js
@@ -3,6 +3,13 @@
 var $panes = $('.pane');
 var $window = $(window);
 
+function initPanes() {
+  $.each($panes, function() {
+    var $pane = $(this);
+    $pane.addClass('in-view');
+  });
+}
+
 function isPaneVisible() {
   var window_height = $window.height();
   var window_top_position = $window.scrollTop();
@@ -15,11 +22,11 @@ function isPaneVisible() {
     var pane_bottom_position = (pane_top_position + pane_height);
 
     //check to see if this current container is within viewport
-    /*if ((pane_bottom_position >= window_top_position) &&
-      (pane_top_position <= window_bottom_position)) {*/
+    if ((pane_bottom_position >= window_top_position) &&
+      (pane_top_position <= window_bottom_position)) {
       $pane.addClass('in-view');
-    //} else {
-    //  $pane.removeClass('in-view');
-    //}
+    } else {
+      $pane.removeClass('in-view');
+    }
   });
 }

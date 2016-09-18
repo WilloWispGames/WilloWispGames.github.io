9924f0b8695635f233d9edf577cd731f29e2c0bd
Jesse Allen <Will-of-the-Wisp@users.noreply.github.com>
2016-09-18 00:46:49 -0400

    Added body fadeout

--- js/projectpanes.js
+++ js/projectpanes.js
@@ -1,6 +1,8 @@
 var $projectpanes = $('.projectpane');
+var $body = $('body');
 
 function initProjectPanes() {
+  $body.addClass('fade-out');
   $.each($projectpanes, function() {
     var $projectpane = $(this);
     $projectpane.addClass('in-view');

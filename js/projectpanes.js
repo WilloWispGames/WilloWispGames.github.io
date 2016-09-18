ad7b2858ef0876592cf29fe2fd5e2ff8a8edca48
Jesse Allen <Will-of-the-Wisp@users.noreply.github.com>
2016-09-18 03:30:51 -0400

    Update projectpanes.js

--- js/projectpanes.js
+++ js/projectpanes.js
@@ -1,30 +1,8 @@
 var $projectpanes = $('.projectpane');
-//var $body = $('body');
 
-function initProjectPanes() {
-  //$body.addClass('fade-out');
+function initProjectPane() {
   $.each($projectpanes, function() {
     var $projectpane = $(this);
     $projectpane.addClass('in-view');
   });
 }
-
-/*
-function changeBackground() {
-  $('intro').animate(
-    {"opacity": 1}, 
-    2000, function(){
-      $('intro').css("opacity", 0);
-    }
-  );
-}
-
-//Auto change Background Image over time
-$(window).load(function() {
-    // Init sequence, loading the first image
-    $('intro').css("opacity", 1);
-    changeBackground();
-
-    //setInterval(changeBackground, 3000);
-});
-*/

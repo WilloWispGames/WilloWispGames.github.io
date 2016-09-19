80c0ce310ec66abfb105674cde697876d9cf97f6
Jesse Allen <Will-of-the-Wisp@users.noreply.github.com>
2016-09-18 23:34:20 -0400

    Removed duplicate code

--- js/projectpanes.js
+++
@@ -1,8 +0,0 @@
-var $projectpanes = $('.projectpane');
-
-function initProjectPane() {
-  $.each($projectpanes, function() {
-    var $projectpane = $(this);
-    $projectpane.addClass('in-view');
-  });
-}

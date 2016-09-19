2081a3e77a9997ca0b7db4f51e12d3a974af54ee
Jesse Allen <Will-of-the-Wisp@users.noreply.github.com>
2016-09-18 23:34:05 -0400

    Removed duplicate code

--- js/contactpanes.js
+++
@@ -1,8 +0,0 @@
-var $contactpanes = $('.contactpane');
-
-function initContactPane() {
-  $.each($contactpanes, function() {
-    var $contactpane = $(this);
-    $contactpane.addClass('in-view');
-  });
-}

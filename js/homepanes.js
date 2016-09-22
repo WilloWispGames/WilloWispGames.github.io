c9c3b72ab599c99a44df56c7b9700b4652e5aed1
Jesse Allen <Will-of-the-Wisp@users.noreply.github.com>
2016-09-21 21:51:16 -0400

    Update homepanes.js

--- js/homepanes.js
+++ js/homepanes.js
@@ -1,10 +1,15 @@
 var $projareas = $('.projarea');
+var $homepanes = $('.homepane');
 var $panes = $('.pane');
 
 function initHomePanes() {
   $.each($projareas, function() {
     var $projarea = $(this);
     $projarea.addClass('in-view');
+  }); 
+  $.each($homepanes, function() {
+    var $homepane = $(this);
+    $homepane.addClass('in-view');
   });  
   $.each($panes, function() {
     var $pane = $(this);
